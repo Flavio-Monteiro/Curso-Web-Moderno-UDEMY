3233580830fb09763234727d90ed5380cc438b90
Flávio Monteiro <flavio.ms@outlook.com.br>
2022-07-04 06:20:58 -0300

    1

---
+++ Exercicios basicos/comentario.js
@@ -0,0 +1,13 @@
+/conseole.log é usado pra exibir algo na tela do computador/
+{
+    console.log(2+5);
+    console.log(2+7);
+    console.log(2+8);
+    console.log(2+9);
+}
+{
+    console.log(2*5);
+    console.log(2*7);
+    console.log(2*8);
+    console.log(2*9);
+}
